d79707ac124466a088a97ebbd4a3d15f39a154cf
Neil Douglas <neil.douglas@york.ac.uk>
2022-11-07 14:30:53 +0000

    update

---
+++ site/profile/manifests/shiny.pp
@@ -0,0 +1,4 @@
+class profile::shiny {
+  class {'shiny':
+  }
+}
---
+++ site/shiny/manifests/init.pp
@@ -0,0 +1,32 @@
+class minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+  $install_dir = '/opt/minecraft',
+){
+  file {$install_dir:
+    ensure => directory,
+
+  }
+  file {"${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $url,
+    before => Service['minecraft'],  
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {"${install_dir}/eula.txt":
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    content => epp('minecraft/minecraft.service', {
+      install_dir => $install_dir,
+    })
+  }
+  service {'minecraft':
+    ensure => false,
+    enable => false,
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
+  }
+}
