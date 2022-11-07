3fae78888f0e30ce05197baf9e80331d5a64c045
Neil Douglas <neil.douglas@york.ac.uk>
2022-11-07 11:34:08 +0000

    update

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,20 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
+  $install_dir = '/opt/minecraft'
+
+  file {'$install_dir':
     ensure => directory,
 
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {"${$install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source => $url,
+    before => Service['minecreaft'],  
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${$install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -21,5 +25,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File["${$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
   }
 }
